89309c8c93128ce77e6840b361d8d27474b122ca
lobosval <t.lobos@duocuc.cl>
2026-05-18 18:15:12 -0400

    cambio

--- front_despacho/src/componentes/CrudAdmin/TableCompras.jsx
+++ front_despacho/src/componentes/CrudAdmin/TableCompras.jsx
@@ -7,7 +7,7 @@ export const TableCompras = () => {
   const [ventas, setVentas] = useState([]);
 
   const compras = async () => {
-    await axios.get("http://10.0.139.209:8081/api/v1/ventas", {
+    await axios.get("/api/v1/ventas", {
       headers:{
         'Content-Type': 'application/json',
         'Accept': 'application/json'
