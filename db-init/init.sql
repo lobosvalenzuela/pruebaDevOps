6bb92916a3f9c05fecad53ac8a76f8483e5f95aa
lobosvalenzuela <t.lobos@duocuc.cl>
2026-05-18 00:07:00 -0400

    cambios base

--- db-init/init.sql
+++ db-init/init.sql
@@ -16,3 +16,16 @@ INSERT INTO DESPACHOS (fechaDespacho, patenteCamion, intento, idCompra, direccio
 ('2024-01-02', 'ABC123', 0, 1, 'Calle 123', 100, FALSE),
 ('2024-02-02', 'DEF456', 0, 2, 'Avenida 456', 200, FALSE),
 ('2024-03-02', 'GHI789', 0, 3, 'Boulevard 789', 150, FALSE);
+
+CREATE TABLE IF NOT EXISTS VENTAS(
+    idVenta INT AUTO_INCREMENT PRIMARY KEY,
+    direccionCompra VARCHAR(255) NOT NULL,
+    valorCompra INT NOT NULL,
+    fechaCompra DATE NOT NULL,
+    despachoGenerado BOOLEAN NOT NULL DEFAULT FALSE
+);
+
+INSERT INTO VENTAS (direccionCompra, valorCompra, fechaCompra, despachoGenerado) VALUES
+('Calle 123', 100, '2024-01-01', FALSE),
+('Avenida 456', 200, '2024-02-01', FALSE),
+('Boulevard 789', 150, '2024-03-01', FALSE);
